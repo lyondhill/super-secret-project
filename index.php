03e9e3d45b4e91a7c47e9c8cc3bede65068ae2e2
Lyon <lyon@delorum.com>
2011-05-06 10:50:28 -0600

    remove extra junk

--- index.php
+++ index.php
@@ -2,20 +2,5 @@
 $name = "Lyon";
 $fart = false;
 ?>
-<h1>WHAT</h1>
 hello <?= $name ?>
 <br />
-my name is 
-<?
-if ($fart)
-{
-	$fart = false;
-	echo "fanny<br />";
-}
-else
-{
-	$fart = true;
-	echo "Tommas<br />";
-}
-
-?>
